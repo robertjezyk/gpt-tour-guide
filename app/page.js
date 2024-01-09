6dead8b8641eca0ffa5745c44805a3c560e900a1
robertjezyk <rjezyk@vp.pl>
2024-01-09 12:44:45 +0000

    changing home page text

--- app/page.js
+++ app/page.js
@@ -5,10 +5,10 @@ const HomePage = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-6xl font-bold text-primary">GPTGenius</h1>
+          <h1 className="text-6xl font-bold text-primary">GPT Tuor Guide</h1>
           <p className="py-6 text-lg leading-loose">
-            GPTGenius: Your AI language companion. Powered by OpenAI, it
-            enhances your conversations, content creation, and more!
+            Your AI journey companion. Powered by OpenAI, it enhances your
+            travels, by providing helpful guides of cities you plan to visit!
           </p>
           <Link href="/chat" className="btn btn-secondary">
             Get Started
